03a011bbe6a97c9d5f66e0f2e77a894401924b3c
islamnabila <81310064+islamnabila@users.noreply.github.com>
2023-09-12 21:48:07 +0600

    learning app for kids

---
+++ lib/main.dart
@@ -0,0 +1,27 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_project_hub/welcome.dart';
+
+void main(){
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: WellcomePage(),
+    );
+  }
+
+}
+
+class MyHomePage extends StatelessWidget{
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+
+    );
+  }
+
+}
